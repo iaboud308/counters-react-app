b203657bdc0dce3212cee745210f7ea3275d8c17
Ibrahim <ibrahim6a@yahoo.co.uk>
2020-04-29 14:52:44 +0100

    Initial Commit

---
+++ build/precache-manifest.5237339e9bd60ce3007977d6484c7a3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c810eb0a314cc225da19e232b181a91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f291161258d9c596ef8d",
+    "url": "/static/css/main.a141594c.chunk.css"
+  },
+  {
+    "revision": "d1fc798089b7d84eb437",
+    "url": "/static/js/2.7e11e77e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7e11e77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f291161258d9c596ef8d",
+    "url": "/static/js/main.733e517d.chunk.js"
+  },
+  {
+    "revision": "af7b873c7a75ce3526f1",
+    "url": "/static/js/runtime-main.36e1cc88.js"
+  }
+]);

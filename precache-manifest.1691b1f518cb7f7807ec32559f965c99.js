144eb31e1b71ac8c9dc3109e8377cf4537cef821
Ibrahim <ibrahim6a@yahoo.co.uk>
2020-05-18 18:21:56 +0100

    Updates

---
+++ precache-manifest.1691b1f518cb7f7807ec32559f965c99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e4e3378cc300c56fc3ff109996c40a",
+    "url": "/counters-react-app/index.html"
+  },
+  {
+    "revision": "b7af67438d484ba10ae2",
+    "url": "/counters-react-app/static/css/main.a141594c.chunk.css"
+  },
+  {
+    "revision": "d1fc798089b7d84eb437",
+    "url": "/counters-react-app/static/js/2.7e11e77e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counters-react-app/static/js/2.7e11e77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7af67438d484ba10ae2",
+    "url": "/counters-react-app/static/js/main.78c9a675.chunk.js"
+  },
+  {
+    "revision": "933b5539e02a7338e57d",
+    "url": "/counters-react-app/static/js/runtime-main.c3a8da06.js"
+  }
+]);
